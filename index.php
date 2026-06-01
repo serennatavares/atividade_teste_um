495ff356e6422f6cad5989571a83e7aee8d41928
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-06-01 12:55:12 -0300

    add - comentários em index

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
 <?php
     session_start();
 
+    // Inicia sessão
+
     include("infra/db/connect.php");
+
+    // Inclui o arquivo que cria a conexão com o banco de dados
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
 
+    // Verifica se o formulário foi enviado utilizando método POST
+
         $usuario = $_POST["usuario"];
         $senha = $_POST["senha"];
+
+        // Armazena os valores do formulário em variáveis
         
         $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
+        // Cria a consulta SQL para verificar se o usuário e senha existem na tabela "usuarios"
+
         $resultado = $conn->query($sql);
 
+        // Executa a consulta e armazena o resultado
+
         if ($resultado->num_rows > 0){
+            // Verifica se a consulta retornou algum resultado (usuário e senha válidos)
             $_SESSION["usuario"] = $usuario;
+            // Armazena o nome do usuário na sessão para manter o usuário logado
             header("Location: public/home.php");
+            // Redireciona o usuário para a página "home.php" após o login bem-sucedido
             exit();
+            // Encerra o script para evitar que o código abaixo seja executado após o redirecionamento
         }else{
             $erro = "Usuário ou senha inválidos!";
+            // Define a variável $erro com uma mensagem de erro para exibir ao usuário
         }
     }
 ?>
@@ -43,8 +60,6 @@
             if(isset($erro)){
                 echo $erro;
             };
-
-            // esse erro serve ara alguma coisa
         
         ?>
         <br>
