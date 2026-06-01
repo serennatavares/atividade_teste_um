0626855e16ebef4dce6b56996decb97ffb6cbc0d
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-06-01 12:58:24 -0300

    add - comentários connect

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,16 +1,24 @@
 <?php
 
     $host = "localhost";
+    // Endereço do servidor do banco de dados (geralmente "localhost" para servidores locais)
     $user = "root";
+    // Nome de usuário do banco de dados (geralmente "root" para servidores locais)
     $pass = "root";
+    // Senha do banco de dados (geralmente "root" para servidores locais)
     $db = "sistema_simples_m1";
+    // Nome do banco de dados que será utilizado
 
     $conn = new mysqli($host,$user,$pass,$db);
+    // Cria uma nova conexão com o banco de dados
 
     if($conn->connect_error){
+        // Verifica se houve um erro na conexão
         die("Erro na conexão!");
+        // Encerra o script e exibe uma mensagem de erro caso a conexão falhe
     }else{
         echo "<script>console.log('Banco conectado com sucesso!')</script>";
+        // Exibe uma mensagem no console do navegador indicando que a conexão foi bem-sucedida
     };
 
 ?>
